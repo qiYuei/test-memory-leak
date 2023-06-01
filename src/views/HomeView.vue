dbe6d2dfffb8225c5d6159058df9210b985cc016
fengmingjian <fmj@hanzhisoft.com>
2023-06-01 14:43:40 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-input-number
+    v-model="num"
+    :min="1"
+    :max="10"
+    @change="handleChange"
+    custom
+    v-for="(i, key) in 20"
+    :key="key"
+  />
+</template>
+
+<script lang="ts" setup>
+import { onBeforeUnmount, ref } from 'vue'
+
+const num = ref(1)
+const handleChange = (value: number) => {
+  console.log(value)
+}
+
+onBeforeUnmount(() => {
+  console.log('unmounting.....')
+})
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
